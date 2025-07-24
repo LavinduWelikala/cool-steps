0d3aa78121d113a43c0592c4a1da48b3284f142b
LavinduWelikala <lavinduwelikala@gmail.com>
2025-07-25 00:02:58 +0530

    some changes

--- src/main/java/com/vihanaus/cool_steps/controller/StepEventController.java
+++ src/main/java/com/vihanaus/cool_steps/controller/StepEventController.java
@@ -1,7 +1,9 @@
 package com.vihanaus.cool_steps.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class StepEventController {
 }
--- src/main/java/com/vihanaus/cool_steps/service/impl/StepEventServiceImpl.java
+++ src/main/java/com/vihanaus/cool_steps/service/impl/StepEventServiceImpl.java
@@ -1,8 +1,10 @@
 package com.vihanaus.cool_steps.service.impl;
 
 import com.vihanaus.cool_steps.service.StepEventService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class StepEventServiceImpl implements StepEventService {
 }
